0f6a756cb6507640a33b0a206afba507f5ab8b72
Travis Stephens <tstephens010@gmail.com>
2019-07-09 17:31:40 -0400

    added the ability to change the title of uploads

--- app/controllers/youtubes_controller.rb
+++ app/controllers/youtubes_controller.rb
@@ -3,13 +3,18 @@ class YoutubesController < ApplicationController
     end
 
     def create
+        snippets = {
+            title: params[:youtube][:title]
+        }.to_s
         youtube_upload_scope = 'https://www.googleapis.com/upload/youtube/v3/videos'
-        payload = Faraday::UploadIO.new(
+        payload = {video: Faraday::UploadIO.new(
             params[:youtube][:youtube].tempfile.path,
             params[:youtube][:youtube].content_type
-        )
+        ),
+        snippet: snippets }.to_s
         
         conn = Faraday.new(url: youtube_upload_scope) do |faraday|
+            faraday.request :multipart
             faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
         end
         res = conn.post do |req|
@@ -21,8 +26,9 @@ class YoutubesController < ApplicationController
         end
 
         if res.status == 200
-            @vid = "https://www.youtube.com/watch?#{JSON.parse(res.body)['id']}"
+            @vid = "https://www.youtube.com/embed/#{JSON.parse(res.body)['id']}"
             render 'youtubes/ph'
+            raise
         else
             raise
         end
